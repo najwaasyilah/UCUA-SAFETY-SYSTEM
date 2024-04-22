176f8adf376d0153cdc33697e7eab3d1bb2c17fc
MusabALT <121367869+MusabALT@users.noreply.github.com>
2024-04-22 23:13:10 +0800

    FIX

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\musab\development\sdks\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\musab\ucua_user_profile"
+export "FLUTTER_APPLICATION_PATH=C:\Users\musab\OneDrive\Documents\GitHub\BIT-BUDDIES"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
