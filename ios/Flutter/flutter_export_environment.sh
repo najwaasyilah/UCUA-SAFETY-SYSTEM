d14075033d827eed93d07729479657530caf25d8
Mnqarlz <aiman020404@gmail.com>
2024-04-25 15:09:53 +0800

    chane name

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\musab\development\sdks\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\musab\OneDrive\Documents\GitHub\BIT-BUDDIES"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\User\Documents\DEGREE\SEM 1\SECJ2154 - OBJECT ORIENTED PROGRAMMING\UCUA-SAFETY-SYSTEM"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
