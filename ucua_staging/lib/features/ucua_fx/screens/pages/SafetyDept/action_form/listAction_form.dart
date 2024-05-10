5adefc70bd38644f8a1e6c78a7e5d182732dad32
MusabALT <121367869+MusabALT@users.noreply.github.com>
2024-05-11 06:32:38 +0800

    Update listAction_form.dart

--- ucua_staging/lib/features/ucua_fx/screens/pages/SafetyDept/action_form/listAction_form.dart
+++ ucua_staging/lib/features/ucua_fx/screens/pages/SafetyDept/action_form/listAction_form.dart
@@ -1,20 +1,6 @@
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Actions List',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: ActionsListPage(),
-    );
-  }
-}
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ActionsListPage extends StatelessWidget {
   @override
@@ -23,46 +9,20 @@ class ActionsListPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('List of Actions'),
       ),
-      body: StreamBuilder<QuerySnapshot>(
+      body: StreamBuilder(
         stream: FirebaseFirestore.instance.collection('actions').snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          }
-
-          if (snapshot.connectionState == ConnectionState.waiting) {
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData)
             return Center(child: CircularProgressIndicator());
-          }
-
-          return ListView(
-            children: snapshot.data!.docs.map((DocumentSnapshot document) {
-              Map<String, dynamic> data =
-                  document.data()! as Map<String, dynamic>;
-              return Card(
-                margin: EdgeInsets.all(8.0),
-                child: ListTile(
-                  title: Text(data['location'] ?? 'No Location'),
-                  subtitle: Padding(
-                    padding: const EdgeInsets.only(top: 8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                            'Offence: ${data['offence'] ?? 'No Offence Code'}'),
-                        Text(
-                            'Action: ${data['action'] ?? 'No Immediate Corrective Action'}'),
-                        Text(
-                            'Violater: ${data['violater'] ?? 'No Violater Name'}'),
-                        Text('Staff ID: ${data['staffId'] ?? 'No Staff ID'}'),
-                        Text('IC/Passport: ${data['ic'] ?? 'No IC/Passport'}'),
-                        Text('Date: ${data['date'] ?? 'No Date'}'),
-                      ],
-                    ),
-                  ),
-                  isThreeLine: true,
-                ),
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              var action = snapshot.data!.docs[index];
+              return ListTile(
+                title: Text(action['location']),
+                subtitle: Text(action['offence']),
               );
-            }).toList(),
+            },
           );
         },
       ),
