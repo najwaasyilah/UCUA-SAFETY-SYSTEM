bb94bcec34b8005cbfa80449057e4de177e87e45
syednopal <110806810+syednopal@users.noreply.github.com>
2024-05-25 00:37:05 +0800

    Create notifications.dart

---
+++ lib/features/ucua_fx/screens/pages/SafetyDept/notifications.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class SafeDeptNotyPage extends StatefulWidget {
+  const SafeDeptNotyPage({Key? key}) : super(key: key);
+
+  @override
+  _SafeDeptNotyPageState createState() => _SafeDeptNotyPageState();
+}
+
+class _SafeDeptNotyPageState extends State<SafeDeptNotyPage> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false, // Remove the back button
+        title: const Text('Notifications'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text(
+          'No Notifications for Safety Department',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(context, "/safetyHome");
+        },
+        backgroundColor: const Color.fromARGB(255, 33, 82, 243),
+        child: const Icon(
+          Icons.home,
+          size: 30,
+          color: Colors.white,
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: const Color.fromARGB(255, 33, 82, 243),
+        unselectedItemColor: Colors.grey,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Notifications',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      switch (index) {
+        case 0:
+          Navigator.pushNamed(context, "/safeDeptNoty");
+          break;
+        case 1:
+          Navigator.pushNamed(context, "/safeDeptProfile");
+          break;
+      }
+    });
+  }
+}
