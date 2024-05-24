73ec431e4f1ec493514e1d7404b5934d2571e50c
syednopal <110806810+syednopal@users.noreply.github.com>
2024-05-25 00:24:14 +0800

    Create notifications.dart

---
+++ lib/features/ucua_fx/screens/pages/Admin/notifications.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class adminNotyPage extends StatefulWidget {
+  const adminNotyPage({Key? key}) : super(key: key);
+
+  @override
+  _adminNotyPageState createState() => _adminNotyPageState();
+}
+
+class _adminNotyPageState extends State<adminNotyPage> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false, // Remove the back button
+        title: Text('Notifications'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          'No Notifications for Admin',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(context, "/adminHome");
+        },
+        backgroundColor: Color.fromARGB(255, 33, 82, 243),
+        child: Icon(
+          Icons.home,
+          size: 30,
+          color: Colors.white,
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: const Color.fromARGB(255, 33, 82, 243),
+        unselectedItemColor: Colors.grey,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Notifications',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      switch (index) {
+        case 0:
+          Navigator.pushNamed(context, "/adminNotyPage");
+          break;
+        case 1:
+          Navigator.pushNamed(context, "/adminProfile");
+          break;
+      }
+    });
+  }
+}
