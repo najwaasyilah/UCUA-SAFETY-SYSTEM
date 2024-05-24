baab929225577f449ce153b2c1ca90dcd65c4a52
syednopal <110806810+syednopal@users.noreply.github.com>
2024-05-25 00:36:35 +0800

    Update navbar.dart

--- lib/features/ucua_fx/screens/pages/SafetyDept/navbar.dart
+++ lib/features/ucua_fx/screens/pages/SafetyDept/navbar.dart
@@ -4,7 +4,7 @@ class NavBar extends StatelessWidget {
   const NavBar({super.key});
 
   @override
-  Widget build(BuildContext context) {
+   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
@@ -46,12 +46,10 @@ class NavBar extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               _buildDropdownItem(context, 'Action Form', '/view_action_form'),
-              _buildDropdownItem(
-                  context, 'View Action Status', '/view_action_status'),
-              _buildDropdownItem(
-                  context, 'View Action List', '/view_action_form_list'),
-              _buildDropdownItem(
-                  context, 'Update Action', '/update_action_form'),
+              //_buildDropdownItem(context, 'View Action Status', '/view_action_status'),
+              _buildDropdownItem(context, 'View Action List', '/view_action_form_list'),
+              //_buildDropdownItem(context, 'Update Action', '/update_action_form'),
+
             ],
           ),
         ),
@@ -69,14 +67,10 @@ class NavBar extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              _buildDropdownItem(
-                  context, 'Condition Form', '/view_condition_form'),
-              _buildDropdownItem(
-                  context, 'View Condition Status', '/view_condition_status'),
-              _buildDropdownItem(
-                  context, 'View Condition List', '/view_condition_form_list'),
-              _buildDropdownItem(
-                  context, 'Update Condition', '/update_condition_form'),
+              _buildDropdownItem(context, 'Condition Form', '/view_condition_form'),
+              //_buildDropdownItem(context, 'View Condition Status', '/view_condition_status'),
+              _buildDropdownItem(context, 'View Condition List', '/view_condition_form_list'),
+              //_buildDropdownItem(context, 'Update Condition', '/update_condition_form'),
             ],
           ),
         ),
