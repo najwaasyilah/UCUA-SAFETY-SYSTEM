11ff5306d859693b751adb3becbc34ecfba02a68
syednopal <110806810+syednopal@users.noreply.github.com>
2024-06-21 02:44:49 +0800

    Update faq.dart

--- lib/features/ucua_fx/screens/pages/Employee/user_profile/faq.dart
+++ lib/features/ucua_fx/screens/pages/Employee/user_profile/faq.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
-class empFAQPage extends StatelessWidget {
-  const empFAQPage({super.key});
+void main() {
+  runApp(MaterialApp(
+    home: EmpFAQPage(),
+  ));
+}
+
+class EmpFAQPage extends StatelessWidget {
+  const EmpFAQPage({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -10,72 +16,130 @@ class empFAQPage extends StatelessWidget {
         title: const Text('FAQ'),
         centerTitle: true,
       ),
-      body: const FAQList(),
+      body: FAQList(),
     );
   }
 }
 
 class FAQList extends StatelessWidget {
-  const FAQList({super.key});
+  const FAQList({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return ListView(
       padding: const EdgeInsets.all(16.0),
-      children: const [
-        FAQItem(
-          question: 'How can I reset my password?',
-          answer:
-              'To reset your password, go to the Profile page and select "Change Password". Enter the old password and then enter the new password',
+      children: [
+        FAQCategory(
+          categoryName: 'Account Management',
+          faqs: [
+            FAQItem(
+              question: 'How can I reset my password?',
+              answer:
+                  'To reset your password, go to the Profile page and select "Change Password". Enter the old password and then enter the new password.',
+            ),
+            FAQItem(
+              question: 'How can I update my profile?',
+              answer:
+                  'To update your profile, go to the Profile page and click on "Edit Profile". Make the necessary changes and save.',
+            ),
+          ],
         ),
-        FAQItem(
-          question: 'How can I update my profile?',
-          answer:
-              'To update your profile, go to the Profile page and click on "Edit Profile". Make the necessary changes and save.',
+        FAQCategory(
+          categoryName: 'Feedback and Notifications',
+          faqs: [
+            FAQItem(
+              question: 'How do I submit feedback?',
+              answer:
+                  'To submit feedback, go to the Feedback page, fill in the form with your details and feedback, and click on the "Submit" button.',
+            ),
+            FAQItem(
+              question: 'Where can I view my notifications?',
+              answer:
+                  'To view your notifications, click on the bell icon on the bottom navigation bar. This will take you to the Notifications page.',
+            ),
+          ],
         ),
-        FAQItem(
-          question: 'How do I submit feedback?',
-          answer:
-              'To submit feedback, go to the Feedback page, fill in the form with your details and feedback, and click on the "Submit" button.',
+        FAQCategory(
+          categoryName: 'Reporting',
+          faqs: [
+            FAQItem(
+              question: 'How do I file a report?',
+              answer:
+                  'To file a report, on the homepage choose between Unsafe Action or Unsafe Condition report and tap on it. Fill in the form with your details.',
+            ),
+            FAQItem(
+              question: 'How do I view all reports?',
+              answer:
+                  'To view all reports that have been made, on the homepage scroll down and you will find two lists of reports.',
+            ),
+          ],
         ),
-        FAQItem(
-          question: 'Where can I view my notifications?',
-          answer:
-              'To view your notifications, click on the bell icon on the bottom navigation bar. This will take you to the Notifications page.',
-        ),
-        FAQItem(
-          question: 'How do I file a report?',
-          answer:
-              'To file a report, on homepage choose between Unsafe Action or Unsafe Condition report and tap on it. Fill in the form with your details',
-        ),
-        FAQItem(
-          question: 'How do I view every reports?',
-          answer:
-              'To view every reports that has been make, on homepage scroll down and you will found two list of reports.',
+        FAQCategory(
+          categoryName: 'Security and Privacy',
+          faqs: [
+            FAQItem(
+              question: 'How is my data secured within the app?',
+              answer:
+                  'We use industry-standard encryption and security protocols to protect your data. For more details, refer to our privacy policy in the "Settings" section.',
+            ),
+          ],
         ),
       ],
     );
   }
 }
 
-class FAQItem extends StatefulWidget {
-  final String question;
-  final String answer;
+class FAQCategory extends StatelessWidget {
+  final String categoryName;
+  final List<FAQItem> faqs;
 
-  const FAQItem({required this.question, required this.answer, super.key});
+  const FAQCategory({
+    required this.categoryName,
+    required this.faqs,
+    Key? key,
+  }) : super(key: key);
 
   @override
-  _FAQItemState createState() => _FAQItemState();
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: Text(
+            categoryName,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 33, 82, 243), // Your specified color
+            ),
+          ),
+        ),
+        Column(
+          children: faqs.map((faq) => FAQItemWidget(faq: faq)).toList(),
+        ),
+      ],
+    );
+  }
 }
 
-class _FAQItemState extends State<FAQItem> {
+class FAQItemWidget extends StatefulWidget {
+  final FAQItem faq;
+
+  const FAQItemWidget({Key? key, required this.faq}) : super(key: key);
+
+  @override
+  _FAQItemWidgetState createState() => _FAQItemWidgetState();
+}
+
+class _FAQItemWidgetState extends State<FAQItemWidget> {
   bool _isExpanded = false;
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ExpansionTile(
-        title: Text(widget.question),
+        title: Text(widget.faq.question),
         trailing: Icon(
           _isExpanded ? Icons.expand_less : Icons.expand_more,
         ),
@@ -87,10 +151,20 @@ class _FAQItemState extends State<FAQItem> {
         children: <Widget>[
           Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Text(widget.answer),
+            child: Text(widget.faq.answer),
           ),
         ],
       ),
     );
   }
+}
+
+class FAQItem {
+  final String question;
+  final String answer;
+
+  FAQItem({
+    required this.question,
+    required this.answer,
+  });
 }
